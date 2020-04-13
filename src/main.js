ed8a63692f5007c0a7a0c21248e1b7397fcd9b12
steven <stv.chm@gmail.com>
2020-04-13 17:07:12 +0800

    add generated js from ulz-data , and redefine proto

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ var messages = require('./proto_js/message_pb');
 var dataStruct = require('./proto_js/Data_pb');
 var services = require('./proto_grpc/service_grpc_pb');
 
+var skill_func_index = require('./skill/skill_data.json');
+
+
 var fs = require('fs');
 var parseArgs = require('minimist');
 var path = require('path');
@@ -11,15 +14,26 @@ var grpc = require('grpc');
 
 
 function _SkillCalculate(call) {
-    var tar_skill = function(){};
+    var tar_skill = [];
     // call : SESkillCalReq
-    // var funcBind = call.
+    var funcBind = call.featList;
+    console.log(funcBind);
+    funcBind.forEach(element => {
+        console.log(element);
+        let exist = _.filter(skill_func_index, function (o) {
+            return o['feat_no'] == element.featNo &&
+                o['id'] == element.id;
+        });
+        // if () {
+        //     let 
+        // }
+    });
 }
 
 function getServer() {
     var server = new grpc.Server();
     server.addService(services.SkillEffectServiceService, {
-        skillCalculate : _SkillCalculate,
+        skillCalculate: _SkillCalculate,
 
     });
     return server;
@@ -29,9 +43,9 @@ if (require.main === module) {
     // If this is run as a script, start a server on an unused port
     var routeServer = getServer();
     routeServer.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-    var argv = parseArgs(process.argv, {
-        string: 'db_path'
-    });
+    // var argv = parseArgs(process.argv, {
+    //     string: 'db_path'
+    // });
     // fs.readFile(path.resolve(argv.db_path), function (err, data) {
     //     if (err) throw err;
     //     // Transform the loaded features to Feature objects
